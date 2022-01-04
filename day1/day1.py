b25628d9f477ed6a5512383fcfcf841347a09581
harshparikh17 <harshparikh882@gmail.com>
2022-01-04 18:44:45 +0530

    Create day1.py
    Day1

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+#Task1
+print("Task1")
+a=int(input("Enter number 1"))
+b=int(input("Enter number 2"))
+sum=a+b
+print(f"Sum of{a} and {b} is {sum}")
+#Task2
+print("Task2")
+a=input("Enter any String")
+b=input("Enter another String")
+result=a+b
+print("Result is",result)
+#Task3
+print("Task3")
+a=float(input("Enter float value"))
+b=float(input("Enter another float value"))
+c=a+b
+print(f"The sum of {a} and {b} is {c}")
