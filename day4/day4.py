eaf4836c7c8b18f445d8ea39148ca2e525080d27
harshparikh17 <harshparikh882@gmail.com>
2022-01-07 18:21:45 +0530

    day4
    day4

---
+++ day4/day4.py
@@ -0,0 +1,38 @@
+#Task1
+print("Task1")
+a=int(input("Enter value 1"))
+b=int(input("Enter value 2"))
+print("Assign(+)=",a+b)
+c=int(input("Enter value 3"))
+d=int(input("Enter value 4"))
+c+=d
+print(f"Add and Assign: c+=d(This will add value of d in  value of c) :",c)
+e=int(input("Enter value 5"))
+f=int(input("Enter value 6"))
+e-=f
+print(f"Subtract and Assign e-=f(This will subtract value of f from value of e) :",e)
+g=int(input("Enter value 7"))
+h=int(input("Enter value 8"))
+g*=h
+print(f"Multiply and Assign g*=h (This is multiply the value of h in value of g)",g)
+i=int(input("Enter value 9"))
+j=int(input("Enter value 10"))
+i/=j
+print(f"Divide and Assign i/=j (This will divide the value of i from value of j)",i)
+#Task2
+print("Task2")
+a=int(input("Enter value 1"))
+b=int(input("Enter value 2"))
+print("The value of a&b(Bitwise And) is :",a&b)
+print("The value of a|b(Bitwise Or) is :",a|b)
+print("The value of ~a(Bitwise Not) is :",~a)
+print("The value of a^b(Bitwise Xor) is :",a^b)
+#Task3
+print("Task3")
+a=True
+b=False
+print(f"{a} and {b} will be : {a and b}")
+print(f" {a} or {b} will be : {a or b} ")
+print(f"{a} not will be : { not a}")
+
+
