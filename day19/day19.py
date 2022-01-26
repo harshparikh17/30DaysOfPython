0b699de8e4921374fc912e3a8894612a2705c620
harshparikh17 <harshparikh882@gmail.com>
2022-01-26 18:01:58 +0530

    day19
    day19

---
+++ day19/day19.py
@@ -0,0 +1,22 @@
+#Task1
+import pandas as pd
+
+GermanCars = {'Company': ['Ford', 'Mercedes', 'BMV', 'Audi'], 'Price': [23845, 171995, 135925 , 71400]}
+carsDf1 = pd.DataFrame.from_dict(GermanCars)
+
+japaneseCars = {'Company': ['Toyota', 'Honda', 'Nissan', 'Mitsubishi '], 'Price': [29995, 23600, 61500 , 58900]}
+carsDf2 = pd.DataFrame.from_dict(japaneseCars)
+
+carsDf = pd.concat([carsDf1, carsDf2], keys=["Germany", "Japan"])
+carsDf
+#Task2
+
+
+Car_Price = {'Company': ['Toyota', 'Honda', 'BMV', 'Audi'], 'Price': [23845, 17995, 135925 , 71400]}
+carPriceDf = pd.DataFrame.from_dict(Car_Price)
+
+car_Horsepower = {'Company': ['Toyota', 'Honda', 'BMV', 'Audi'], 'horsepower': [141, 80, 182 , 160]}
+carsHorsepowerDf = pd.DataFrame.from_dict(car_Horsepower)
+
+carsDf = pd.merge(carPriceDf, carsHorsepowerDf, on="Company")
+carsDf
